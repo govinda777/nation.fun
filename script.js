90256328b3c2f5c97f6330b69e12b750edf4f697
Govinda <govinda777@gmail.com>
2025-11-01 11:25:11 -0300

    Revert "Implement BFF to Secure API Token"

--- script.js
+++ script.js
@@ -20,12 +20,14 @@ function sendMessage() {
     addMessage(message, 'user');
     messageInput.value = '';
 
-    fetch('/api/chat', {
+    fetch('https://open.service.crestal.network/v1/chat/completions', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${window.TOKEN_NATION}`
         },
         body: JSON.stringify({
+            model: 'intentkit-001',
             messages: [{
                 role: 'user',
                 content: message
